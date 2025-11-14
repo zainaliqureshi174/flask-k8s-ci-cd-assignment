669836b7aa5fba67c0953742b91e1dc154cc103c
Zain Qureshi <quraishizain500@gmail.com>
2025-11-14 20:50:07 +0500

    Add GitHub Actions CI with flake8 and pytest

---
+++ test_app.py
@@ -0,0 +1,7 @@
+from app import app
+
+def test_hello():
+    client = app.test_client()
+    response = client.get('/')
+    assert response.status_code == 200
+    assert b"Hello, World!" in response.data
