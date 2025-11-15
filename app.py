a60a8355feddfd3d506bc0e976b3bc07cc16e17c
Zain Qureshi <quraishizain500@gmail.com>
2025-11-15 21:59:02 +0500

    Fix blank lines for flake8

--- app.py
+++ app.py
@@ -1,12 +1,14 @@
 from flask import Flask
 
+
 app = Flask(__name__)
 
 
 @app.route('/')
+
 def hello():
     return "Hello, World! Flask K8s CI/CD Pipeline"
-    
-    
+
+
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=5000)
